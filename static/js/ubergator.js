815aa538f4c089959c46c7dbd6f35cee5c8b0bf9
gregory80 <gregory.tomlinson@gmail.com>
2011-10-14 22:51:20 -0400

    move facebook data, create tab parser ajax display of current cities from facebook.txt

--- static/js/ubergator.js
+++ static/js/ubergator.js
@@ -6,9 +6,28 @@
 *
 *
 * */
+;(function(win) {
 
+
+
+var tabstop_regex = /\t/;
+function handle_tab_text( txt_str ) {
+    var lines = txt_str.split(/\r|\n/);
+    var i=lines.length, city_data, data=[];
+    while(i--) {
+        city_data = lines && lines[i].split( tabstop_regex );
+        if(!city_data[0] || !city_data[1]) continue;
+        city_data && data.push({
+            url:city_data[0],
+            text:city_data[1]
+        });
+
+    }
+    return data.reverse();
+}
 var ubergator = {
     save_url:"/data/save/endpoint",
+    cities_url:"data/facebook.txt",
     start:function() {
         this.dom.saveTemplates();
         
@@ -26,7 +45,6 @@ var ubergator = {
         elem.find("form").bind("submit", function(e) {
             e.preventDefault();
             var inputs = elem.find("input[type='text']");
-            console.log(inputs);
             var promise = self.saveRss({
                 url:inputs.val()
             });
@@ -37,24 +55,50 @@ var ubergator = {
             });
         });
     },
+    addCitiesList:function($el) {
+        var promise = this.getCities();
+        promise.done(function(data) {
+            var city_data = handle_tab_text(data);
+            var frag = newT.render("showCities.ubergator", {
+                "city_data":city_data,
+                pagination:{
+                    start:1,
+                    limit:10,
+                    total:10000
+                }
+            });
+            $el.append(frag);
+        });
+    },
+    getCities:function() {
+        var promise = this.remoteCall(this.cities_url, {}, {
+            dataType:"text"
+        });
+        return promise;
+    },
     saveRss:function( data ) {
-        var promise = this.remoteCall( this.save_url, data );
+        var promise = this.remoteCall( this.save_url, data, {
+            type:"GET"
+        });
         promise.done(function(data) {
             console.log("I got data!", data);
         });
         return promise;
     },
 
-    remoteCall:function( url, params ) {
+    remoteCall:function( url, params, opts ) {
         var dfr = $.Deferred();
-        jQuery.ajax({
+        var basics = {
             url:url,
-            method:"GET",
+            type:"GET",
             dataType:"json",
             success:dfr.resolve,
             error:dfr.resolve,
-            data:params
-        });
+            data:null
+        };
+        basics = $.extend({}, basics, opts || {});
+        if(!basics.data) basics.data=params;
+        jQuery.ajax(basics);
         return dfr.promise();
     }
 
@@ -75,5 +119,34 @@ ubergator.dom = {
                 )
             );
         });
+        newT.save("showCities.ubergator", function(data) {
+            return (
+                newT.div(
+                    newT.eachRender(data.city_data, "city.ubergator"),
+                    // show the pagination
+                    newT.render("cityPagination.ubergator", data.pagination)
+                )
+            )
+        });
+        newT.save("cityPagination.ubergator", function(data) {
+            return (
+                newT.div()
+            );
+        });
+        // called via newT.eachRender
+        newT.save("city.ubergator", function(data) {
+            return (
+                newT.div({clss:"ug_cityItem"},
+                    newT.a({
+                        href:data.url
+                    }, data.text || "")
+                )
+            )
+        });
     }
 }
+
+
+// expose to global object
+win.ubergator=ubergator;
+})(window);
